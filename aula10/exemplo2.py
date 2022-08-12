e85a6fd502ab8af12d7a8001e0f23086d8961452
Erich Brendell <erychbrendell@gmail.com>
2022-08-12 14:21:41 -0300

    creation:exemplo2.py in aula10

---
+++ aula10/exemplo2.py
@@ -0,0 +1,17 @@
+"""
+estrutura de repetição + estrutura condicional
+(for + if/elif/else)
+"""
+
+texto = 'Python'
+nova_string = ''
+
+for letra in texto:
+    if letra == 't':
+        nova_string += letra.upper()
+    elif letra == 'h':
+        nova_string += letra.upper()
+    else:
+        nova_string += letra
+
+print(nova_string)
