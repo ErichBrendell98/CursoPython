75329c91bb3fff868a8f2980e543ec14e7eeb4de
Erich Brendell <erychbrendell@gmail.com>
2022-08-12 14:11:04 -0300

    creation:aula10&exemplo1.py

--- aula10/exemplo1.py
+++ aula10/exemplo1.py
@@ -3,3 +3,39 @@ For in em Python
 Iterando strings com for
 Função range(start = 0, stop, step=1)
 """
+
+texto = 'Python'
+
+for letra in texto:
+    print(letra)
+
+print()
+
+for n in range(10):
+    print(n)
+
+print()
+
+for n in range(5, 10):
+    print(n)
+
+print()
+
+for n in range(0, 10, 2):
+    print(n)
+
+print()    
+
+for n in range(20, 10, -1):
+    print(n)
+
+print()
+
+for n in range(0, 100, 8):
+    print(n)
+
+print()
+
+for n in range(100):
+    if n % 8 == 0:
+        print(n)
